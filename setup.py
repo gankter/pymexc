0c78947ad0fc8bf2cdfa4eaf1c67d449441e47b7
Mikita <nikita7828@gmail.com>
2025-11-11 00:57:09 +0300

    refactor

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
             "**/*.pyi",
             "web/*",
             "_async/*",
-            "models/*"
+            "models/*",
             "proto/*",
         ]
         for package in find_packages()
